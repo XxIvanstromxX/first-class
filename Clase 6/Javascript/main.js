15c65249162c97cbaf1145341a4256b150719566
amembrillo9 <amembrillo9@gmail.com>
2025-05-15 05:19:41 +0000

    Clase 6 javascript

---
+++ Clase 6/Javascript/main.js
@@ -0,0 +1,37 @@
+//DECLARACION DE VARIABLES
+
+/*
+let nombre = "Jose"; //Tipo de variable reasignable
+const edad = 22;     //Tipo de variable que no permite reasignación
+*/
+
+//TIPOS DE DATOS BÁSICOS (datos primitivos)
+
+lecadent a = "JAVASCRIPT EXTERNO";      // String
+let numero = 10;                        // Number
+let decimal = 3.1416;                   // Float/Number
+let booleano = true;                    // Boolean
+let indefinido;                         // Undefined
+let nulo = null;                        // nulo o null
+
+console.log(typeof cadena);
+console.log(typeof numero);
+console.log(typeof decimal);
+console.log(typeof booleano);
+console.log(typeof indefinido);
+console.log(typeof nulo);
+
+//Operadores (Aritmeticas)
+
+let a = 5;
+let b = 10;
+let c = 25;
+
+console.log(a + b);  //Suma = 15
+console.log(b - a);  //Resta = 5
+console.log(a * b);  //Multi = 50;
+console.log(b / a);  //Divi = 2
+console.log(b % a);  //Modulo = 0
+console.log( a ** 2); //potencia
+console.log("la raiz cuadrada de 25 es: ")
+console.log( c ** 0.5);
