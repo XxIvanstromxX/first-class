de2c47c07ad891488bb8ee04834ee633e04907b7
amembrillo9 <amembrillo9@gmail.com>
2025-05-17 04:46:49 +0000

    Operadores de compracion clase 7

---
+++ Clase 7/Operadores de comparacion/script.js
@@ -0,0 +1,34 @@
+let a = 12;
+let b = 12;
+
+let doble_igualdad = a == b;
+
+console.log("La doble igualdad es: ", doble_igualdad);
+
+let triple_igualdad = a === b;
+
+console.log("La triple igualdad es:", triple_igualdad);
+
+let menor_que = a < b;
+
+console.log("La comparación menor que es: ", menor_que);
+
+let mayor_que = a > b;
+
+console.log("La comparación mayor que es: ", mayor_que);
+
+let menor_igual_que = a <= b;
+
+console.log("La comparación menor o igual que es: ", menor_igual_que);
+
+let mayor_igual_que = a >= b;
+
+console.log("La comparación mayor o igual que es: ", mayor_igual_que);
+
+let distinto_de = a != b;
+
+console.log("La comparación distinto de es: ", distinto_de);
+
+let distinto_de_igualdad = a !== b;
+
+console.log("La comparación distinto de igualdad es: ", distinto_de_igualdad);
