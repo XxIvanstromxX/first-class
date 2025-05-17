ee82c835b7afa142841e5f5fe4e6fac6eeaa4674
amembrillo9 <amembrillo9@gmail.com>
2025-05-17 04:47:26 +0000

    Condicionales clase 7

---
+++ Clase 7/condicionales/script.js
@@ -0,0 +1,20 @@
+let edad = 16;
+
+if (edad >= 18) {
+    console.log("Eres mayor de edad");
+}else if(edad == 17){
+    console.log("Ya casi eres mayor de edad");
+}else{
+    console.log("Eres menor de edad");
+}
+
+
+let hora = 14;
+
+if(hora < 12){
+    console.log("Buenos días");
+}else if(hora < 18){
+    console.log("Buenas tardes");
+}else{
+    console.log("Buenas noches");
+}
