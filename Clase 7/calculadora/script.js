63af15a1085cde430fa69ef908869ceaaf98c082
amembrillo9 <amembrillo9@gmail.com>
2025-05-20 03:34:02 +0000

    Mejora de calculadora

--- Clase 7/calculadora/script.js
+++ Clase 7/calculadora/script.js
@@ -14,9 +14,39 @@ function sumar(){
 const resta = () => {
     let numero1 = Number(document.getElementById("numero1").value);
     let numero2 = Number(document.getElementById("numero2").value);
+    console.log("El numero 1 es", numero1);
+    console.log("El numero 2 es", numero2);
     
     let resultado = numero1 - numero2;
+    console.log("El resultado es", resultado);
 
     document.getElementById("resultado").textContent = "Resultado de la resta es: " + resultado;
     console.log("La resta se hizo correctamente")
 }
+
+const multi = () => {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+    console.log("El numero 1 es", numero1);
+    console.log("El numero 2 es", numero2);
+    
+    let resultado = numero1 * numero2;
+    console.log("El resultado es", resultado);
+
+    document.getElementById("resultado").textContent = "Resultado de la multiplicación es: " + resultado;
+    console.log("La Multiplicación se hizo correctamente")
+}
+
+const divi = () => {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+    console.log("El numero 1 es", numero1);
+    console.log("El numero 2 es", numero2);
+    
+    let resultado = numero1 / numero2;
+    console.log("El resultado es", resultado);
+
+    document.getElementById("resultado").textContent = "Resultado de la división es: " + resultado;
+    console.log("La División se hizo correctamente")
+}
+
