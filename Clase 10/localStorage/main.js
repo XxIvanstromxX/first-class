67233f9ba5bbc596fc0e9f95b85ac34b09a2c388
amembrillo9 <amembrillo9@gmail.com>
2025-05-24 05:37:30 +0000

    Añadiendo comentarios a local storage

--- Clase 10/localStorage/main.js
+++ Clase 10/localStorage/main.js
@@ -1,14 +1,12 @@
+//Crear un elemento en nuestro Local Storage
 localStorage.setItem("nombre", "ivan");
 
+//Obtener un elemento del Local Storage
 let value = localStorage.getItem("nombre");
 
 console.log(value);
-
 const elemento = document.getElementById("primer");
-
 elemento.innerHTML = value;
 
+//Eliminar un elemento del Local Storage
 localStorage.removeItem("nombre");
-
-let value2 = localStorage.getItem("nombre");
-console.log(value2);
